bc4687fa68b11868ff7c40a9b8672a7df4f4de3d
DominikKoperkiewicz <62205572+DominikKoperkiewicz@users.noreply.github.com>
2020-03-16 13:32:33 +0100

    Add files via upload

---
+++ src/LZespolona.cpp
@@ -0,0 +1,74 @@
+#include "LZespolona.hh"
+
+
+
+
+LZespolona  con(LZespolona Z1)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Z1.re;
+  Wynik.im = Z1.im * (-1);
+  return Wynik;
+}
+
+double abs(LZespolona Z1)
+{
+    double Wynik = sqrt(Z1.re * Z1.re + Z1.im * Z1.im);
+    return Wynik;
+}
+
+/*!
+ * Realizuje dodanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik dodawania,
+ *    Skl2 - drugi skladnik dodawania.
+ * Zwraca:
+ *    Sume dwoch skladnikow przekazanych jako parametry.
+ */
+
+LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re + Skl2.re;
+  Wynik.im = Skl1.im + Skl2.im;
+  return Wynik;
+}
+
+LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re - Skl2.re;
+  Wynik.im = Skl1.im - Skl2.im;
+  return Wynik;
+}
+
+LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re * Skl2.re - Skl1.im * Skl2.im;
+  Wynik.im = Skl1.im * Skl2.re + Skl1.re * Skl2.im;
+  return Wynik;
+}
+
+/*
+LZespolona  operator = (LZespolona  &Skl1,  LZespolona  Skl2)
+{
+  &Skl1.re = Skl2.re;
+  &Skl1.im = Skl2.im;
+  return Skl1;
+}
+*/
+
+LZespolona  operator / (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona Wynik;
+  Wynik = Skl1 * con(Skl2);
+  Wynik.re = Wynik.re/( abs(Skl2)*abs(Skl2) );
+  Wynik.im = Wynik.im/( abs(Skl2)*abs(Skl2) );
+
+  return Wynik;
+}
---
+++ src/WyrazenieZesp.cpp
@@ -0,0 +1,26 @@
+#include "WyrazenieZesp.hh"
+
+
+/*
+ * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
+ * w pliku naglowkowym.
+ */
+void Wyswietl(WyrazenieZesp  WyrZ)
+{
+    cout << "(" << WyrZ.Arg1.re << showpos << WyrZ.Arg1.im << noshowpos << "i)";
+    cout << "(" << WyrZ.Arg2.re << showpos << WyrZ.Arg2.im << noshowpos << "i)";
+
+}
+
+LZespolona Oblicz(WyrazenieZesp  WyrZ)
+{
+    LZespolona Z1;
+
+    switch(WyrZ.Op)
+    {
+        case Op_Dodaj  : return WyrZ.Arg1 + WyrZ.Arg2;
+        case Op_Odejmij: return WyrZ.Arg1 - WyrZ.Arg2;
+        case Op_Mnoz   : return WyrZ.Arg1 * WyrZ.Arg2;
+        case Op_Dziel  : return WyrZ.Arg1 / WyrZ.Arg2;
+    }
+}
